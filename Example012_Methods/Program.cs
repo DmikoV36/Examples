6711d33e446eacf6dd06a79525232ccbdff83d4b
Dmitriy <Dmiko@bk.ru>
2022-08-14 12:47:05 +0300

    Example012 Methods

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -33,15 +33,25 @@ int Method3()
 //Console.WriteLine(year);
 
 // Вид 4
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
 string Method4(int count, string text)
 {
-    int i = 0;
-    string result = string.Empty;
-
-    while (i < count)
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
     {
         result = result + text;
-        i++;
     }
     return result;
 }
